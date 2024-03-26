08b0a4c430eac920eb8deada3d8f7a2962417dfa
t0stiman <18124323+t0stiman@users.noreply.github.com>
2024-03-26 08:49:28 +0100

    forgot to commit Stuff.cs

---
+++ src/Stuff.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace better_mouse_mod;
+
+public static class Stuff
+{
+	/// <summary>
+	/// Same as the modulo operator except it works for negative numbers, like in C++.
+	/// </summary>
+	public static int CPPModulo(float a,float b)
+	{
+		return Mathf.RoundToInt(a - b * Mathf.Floor(a / b));
+	}
+}
