d663f8b5670fb97992af57f972f2b311def60f73
t0stiman <18124323+t0stiman@users.noreply.github.com>
2024-04-09 20:15:37 +0200

    boring code stuff

--- src/Patches/TrainInput_Patch.cs
+++ src/Patches/TrainInput_Patch.cs
@@ -29,106 +29,103 @@ public class TrainInput_Update_Patch
 	//i should really learn to transpile
 	private static void Yes(ref TrainInput __instance)
 	{
-		BaseLocomotive loco;
-		LocomotiveControlAdapter adapter;
-
-		if (!__instance.TryGetLocomotiveControlAdapter(out loco, out adapter))
+		if (!__instance.TryGetLocomotiveControlAdapter(out var loco, out var adapter))
 		{
 			return;
 		}
 
-		var shared = GameInput.shared;
-		int num1 = GameInput.IsShiftDown ? 1 : 0;
-		bool isControlDown = GameInput.IsControlDown;
-		float num2 = num1 != 0 ? 0.05f : 0.01f;
-		float reverserDelta = 0.0f;
-		float num4 = 0.0f;
-		float num5 = 0.0f;
-		float num6 = 0.0f;
+		var gameInput = GameInput.shared;
+		var isControlDown = GameInput.IsControlDown;
 		
-		//  ============ start changed ============
+		//todo step size instead of repeat
+		
+		var reverserDelta = 0.0f;
+		var throttleDelta = 0.0f;
+		var num5 = 0.0f;
+		var num6 = 0.0f;
 
-		bool useBigStep = loco.Archetype == CarArchetype.LocomotiveDiesel | isControlDown;
-		float stepSize = useBigStep ? 1f : 0.1f;
+		var useBigStep = loco.Archetype == CarArchetype.LocomotiveDiesel | isControlDown;
+		var reverserStepSize = useBigStep ? 1f : 0.1f;
 
 		var reverserRepeatInterval = Main.MySettings.Notched_RepeatInterval;
 		
-		if (shared.GetReverserBack(reverserRepeatInterval))
+		if (gameInput.GetReverserBack(reverserRepeatInterval))
 		{
-			reverserDelta = -stepSize;
+			reverserDelta = -reverserStepSize;
 		}
-		else if (shared.GetReverserForward(reverserRepeatInterval))
+		else if (gameInput.GetReverserForward(reverserRepeatInterval))
 		{
-			reverserDelta = stepSize;
+			reverserDelta = reverserStepSize;
 		}
 		
-		int throttleNotches = adapter.ThrottleNotches;
+		var throttleStepSize = GameInput.IsShiftDown ? 0.05f : 0.01f;
+		var throttleNotches = adapter.ThrottleNotches;
 		if (throttleNotches > 0) //notched
 		{
-			int num8 = 0;
-			if (shared.GetThrottleDown(Main.MySettings.Notched_RepeatInterval))
+			var throttleChangeo = 0;
+			if (gameInput.GetThrottleDown(Main.MySettings.Notched_RepeatInterval))
 			{
-				num8 = -1;
+				throttleChangeo = -1;
 			}
-			else if (shared.GetThrottleUp(Main.MySettings.Notched_RepeatInterval))
+			else if (gameInput.GetThrottleUp(Main.MySettings.Notched_RepeatInterval))
 			{
-				num8 = 1;
+				throttleChangeo = 1;
 			}
 
-			if (num8 > 0 && adapter.AbstractThrottle < 1.0)
+			if (throttleChangeo > 0 && adapter.AbstractThrottle < 1.0)
 			{
 				__instance.ChangeValue(PropertyChange.Control.Throttle,
 					Mathf.Clamp01(adapter.AbstractThrottle + 1f / throttleNotches));
 			}
 
-			if (num8 < 0 && adapter.AbstractThrottle > 0.0)
+			if (throttleChangeo < 0 && adapter.AbstractThrottle > 0.0)
 			{
 				__instance.ChangeValue(PropertyChange.Control.Throttle,
 					Mathf.Clamp01(adapter.AbstractThrottle - 1f / throttleNotches));
 			}
 		}
 		
-		else if (shared.GetThrottleDown(Main.MySettings.Smooth_RepeatInterval))
+		else if (gameInput.GetThrottleDown(Main.MySettings.Smooth_RepeatInterval))
 		{
-			num4 = -num2;
+			throttleDelta = -throttleStepSize;
 		}
-		else if (shared.GetThrottleUp(Main.MySettings.Smooth_RepeatInterval))
+		else if (gameInput.GetThrottleUp(Main.MySettings.Smooth_RepeatInterval))
 		{
-			num4 = num2;
+			throttleDelta = throttleStepSize;
 		}
 
 		//  ============ end changed ============
 		
 		// train brakes are handled in GameInput_Patch.cs
 		
-		if (shared.TrainBrakeRelease)
-			num5 = (float) (-(double) num2 * 2.0);
-		else if (shared.TrainBrakeApply)
-			num5 = num2;
-		if (shared.LocomotiveBrakeRelease)
-			num6 = (float) (-(double) num2 * 2.0);
-		else if (shared.LocomotiveBrakeApply)
-			num6 = num2;
+		if (gameInput.TrainBrakeRelease)
+			num5 = (float) (-(double) throttleStepSize * 2.0);
+		else if (gameInput.TrainBrakeApply)
+			num5 = throttleStepSize;
+		if (gameInput.LocomotiveBrakeRelease)
+			num6 = (float) (-(double) throttleStepSize * 2.0);
+		else if (gameInput.LocomotiveBrakeApply)
+			num6 = throttleStepSize;
 		if ((double) reverserDelta != 0.0)
 			__instance.ChangeValue(PropertyChange.Control.Reverser, Mathf.Clamp(adapter.AbstractReverser + reverserDelta, -1f, 1f));
 		if ((double) __instance._locomotiveBrakeDelta < 0.0 && (double) adapter.LocomotiveBrakeSetting < 0.0 && (double) num6 == 0.0)
 			__instance.ChangeValue(PropertyChange.Control.LocomotiveBrake, 0.0f);
 		__instance._locomotiveBrakeDelta = num6;
 		__instance._trainBrakeDelta = num5;
-		__instance._throttleDelta = num4;
-		if (shared.Bell)
+		__instance._throttleDelta = throttleDelta;
+		if (gameInput.Bell)
 			StateManager.ApplyLocal((IGameMessage) new PropertyChange(loco.id, PropertyChange.Control.Bell, !loco.locomotiveControl.Bell));
-		if (shared.CylinderCock)
+		if (gameInput.CylinderCock)
 		{
-			bool boolValue = loco.KeyValueObject[PropertyChange.KeyForControl(PropertyChange.Control.CylinderCock)].BoolValue;
+			var boolValue = loco.KeyValueObject[PropertyChange.KeyForControl(PropertyChange.Control.CylinderCock)].BoolValue;
 			StateManager.ApplyLocal((IGameMessage) new PropertyChange(loco.id, PropertyChange.Control.CylinderCock, !boolValue));
 		}
-		float num9 = shared.InputHorn;
-		if (shared.HornExpressionEnabledThisFrame)
-			__instance._hornDownMousePosition = shared.HornExpressionValue;
-		if (shared.HornExpressionEnabled)
+		var num9 = gameInput.InputHorn;
+		if (gameInput.HornExpressionEnabledThisFrame)
+			__instance._hornDownMousePosition = gameInput.HornExpressionValue;
+		if (gameInput.HornExpressionEnabled)
 		{
-			__instance._hornDownMousePosition += shared.HornExpressionValue;
+			__instance._hornDownMousePosition += gameInput.HornExpressionValue;
 			num9 = Mathf.Clamp01((float) (-(double) __instance._hornDownMousePosition / 200.0));
 		}
 		if ((double) Math.Abs(num9 - __instance._hornWas) > 0.0099999997764825821)
@@ -136,7 +133,7 @@ public class TrainInput_Update_Patch
 			StateManager.ApplyLocal((IGameMessage) new PropertyChange(loco.id, PropertyChange.Control.Horn, num9));
 			__instance._hornWas = num9;
 		}
-		int inputHeadlight = shared.InputHeadlight;
+		var inputHeadlight = gameInput.InputHeadlight;
 		if (inputHeadlight == 0)
 			return;
 		Console.Log("Headlight: " + HeadlightToggleLogic.TextForState(HeadlightToggleLogic.SetHeadlightStateOffset(loco.KeyValueObject, inputHeadlight)));
